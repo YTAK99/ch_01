7e0371aceb6f69351588abbdf7a269650b82464f
YTAK99 <ytzzang99@gmail.com>
2026-04-01 18:40:48 +0900

    ㄷㄷ

--- step_2_2.py
+++ step_2_2.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from step_2_1 import WORK_DIR       # 이전 단계에서 작성한 모듈 불러오기
 
-def get_total_filesize(base_dir: Path, pattern: str = "*") -> int:          # 매개변수 base_dir은 Path 객체를, pattern은 글로브 패턴 문자열을 저장장
+def get_total_filesize(base_dir: Path, pattern: str = "*") -> int:          # 매개변수 base_dir은 Path 객체를, pattern은 글로브 패턴 문자열을 저장
     total_bytes = 0                         # total_bytes 초기화. 여기에 파일 크기 저장할 것임
     for fullpath in base_dir.glob(pattern):         # glob()는 입력값으로 전달된 글로브 패턴과 일치하는 파일명을 리스트로 반환
         if fullpath.is_file():          # is_file() 함수를 사용하여 파일이 유효한지 검사
